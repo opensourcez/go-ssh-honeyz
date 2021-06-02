33ddb6d017f73f6ac7bbf07fe201bfc5150f1c8e
zveinn <anon@zkynet.io>
2021-06-02 14:29:24 +0000

    adding keyfile as argument 2

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
 			remoteAddr := c.RemoteAddr().String()
 			ip := remoteAddr[0:strings.Index(remoteAddr, ":")]
-			log.Printf("SSH connection from ip=[%s], username=[%s], password=[%s], version=[%s]", ip, c.User(), pass, c.ClientVersion())
+			log.Printf("ip=[%s], username=[%s], password=[%s], version=[%s]", ip, c.User(), pass, c.ClientVersion())
 			return nil, fmt.Errorf("invalid credentials")
 		},
 	}
 
-	privateBytes, err := ioutil.ReadFile("sshkey")
+	privateBytes, err := ioutil.ReadFile(os.Args[2])
 	if err != nil {
 		log.Fatalf("Failed to load private key.  Run make gen_ssh_key")
 	}
